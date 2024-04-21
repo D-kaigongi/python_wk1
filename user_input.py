9cc36cd065e1612c99f4b9b1cc13012176e2f378
D-kaigongi <mwendadoreen2@gmail.com>
2024-04-21 19:39:18 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input(" What is your name: ")
+age = input(" How old are you?: ")
+location = input(" where do you live?: ")
+
+information =(name, age, location)
+
+print(" Dear ", information[0], "of age ", information[1],"living in ", information[2], " you are beautiful.")
