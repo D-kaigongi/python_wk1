1b5d709f0847ae48d4f5a17088a97c7b0c214b3b
D-kaigongi <mwendadoreen2@gmail.com>
2024-04-21 19:45:38 +0300

    add name

--- user_input.py
+++ user_input.py
@@ -5,3 +5,4 @@ location = input(" where do you live?: ")
 information =(name, age, location)
 
 print(" Dear ", information[0], "of age ", information[1],"living in ", information[2], " you are beautiful.")
+print(name)
